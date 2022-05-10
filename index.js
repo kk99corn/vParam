2ab73b4eadaa24300ace4810afc63cc8bad38a7b
99 <hahahoho5915@gmail.com>
2022-05-10 21:18:18 +0900

    기능변경: js var -> let 변경

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function getParam() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, function (tabs) {
-        var url = tabs[0].url;
+        let url = tabs[0].url;
 
-        var currentUrl = url;
-        var urlText = "";
-        var parameterText = "";
+        let currentUrl = url;
+        let urlText = "";
+        let parameterText = "";
         if (currentUrl.indexOf('?') !== -1) {
             urlText = currentUrl.substring(0, currentUrl.indexOf('?'));
             parameterText = currentUrl.substring(currentUrl.indexOf('?') + 1, currentUrl.length);
@@ -13,14 +13,14 @@ function getParam() {
         }
         document.getElementById("urlText").innerHTML = urlText;
 
-        var parameterList = parameterText.split('&');
-        var innerHtml = "";
-        var paramIndex = 0;
-        for (var i_1 = 0; i_1 < parameterList.length; i_1++) {
+        let parameterList = parameterText.split('&');
+        let innerHtml = "";
+        let paramIndex = 0;
+        for (let i_1 = 0; i_1 < parameterList.length; i_1++) {
             if (parameterList[i_1] !== '') {
-                var paramKV = parameterList[i_1].split('=');
-                var paramK = (paramKV[0]) ? decodeURIComponent(paramKV[0]) : "";
-                var paramV = (paramKV[1]) ? decodeURIComponent(paramKV[1]) : "";
+                let paramKV = parameterList[i_1].split('=');
+                let paramK = (paramKV[0]) ? decodeURIComponent(paramKV[0]) : "";
+                let paramV = (paramKV[1]) ? decodeURIComponent(paramKV[1]) : "";
                 innerHtml += "\n" +
                     "                <tr scope=\"row\">\n" +
                     "                    <th scope=\"row\">\n" +
@@ -37,8 +37,8 @@ function getParam() {
 }
 
 function allCehck() {
-    checkboxParamList = document.querySelectorAll('input[id^="c_"]');
-    for (var i = 0; i < checkboxParamList.length; i++) {
+    let checkboxParamList = document.querySelectorAll('input[id^="c_"]');
+    for (let i = 0; i < checkboxParamList.length; i++) {
         if (document.getElementById("allCheck").checked) {
             checkboxParamList[i].checked = true;
         } else {
@@ -48,25 +48,32 @@ function allCehck() {
 }
 
 function makeURI() {
-    var url = document.getElementById("urlText").innerHTML;
-    var paramString = "?";
-    checkboxParamList = document.querySelectorAll('input[id^="c_"]');
-    var paramCount = 0;
-    for (var i = 0; i < checkboxParamList.length; i++) {
+    let url = document.getElementById("urlText").innerHTML;
+    let paramString = "?";
+    let checkboxParamList = document.querySelectorAll('input[id^="c_"]');
+    let paramCount = 0;
+    for (let i = 0; i < checkboxParamList.length; i++) {
         if (checkboxParamList[i].checked) {
-            var key = document.getElementById("k_" + i).value;
-            var value = document.getElementById("v_" + i).value;
+            let key = document.getElementById("k_" + i).value;
+            let value = document.getElementById("v_" + i).value;
             if (paramCount !== 0) paramString += '&';
             paramString += encodeURIComponent(key) + "=" + encodeURIComponent(value);
             paramCount++;
         }
     }
     document.getElementById("makeURI").value = url + paramString;
+
+    // url redirect
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function (tabs) {
+        chrome.tabs.update(
+            {url: url + paramString}
+        );
+    });
 }
 
 function paramPlus() {
-    var insertIndex = document.querySelectorAll('input[id^="c_"]').length;
-    var innerHtml = "";
+    let insertIndex = document.querySelectorAll('input[id^="c_"]').length;
+    let innerHtml = "";
     innerHtml += "\n" +
         "                <tr scope=\"row\">\n" +
         "                    <th scope=\"row\">\n" +
@@ -79,9 +86,9 @@ function paramPlus() {
 }
 
 function appendHtml(el, str) {
-    var table = document.createElement('table');
+    let table = document.createElement('table');
     table.innerHTML = str;
-    var tbody = table.children[0];
+    let tbody = table.children[0];
     while (tbody.children.length > 0) {
         el.appendChild(tbody.children[0]);
     }
